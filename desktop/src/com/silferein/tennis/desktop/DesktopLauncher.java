f4ce219c3100a73a8438a29ed1fa62fa2fa87c34
John Lynn <lynnjonathanp@gmail.com>
2015-11-27 16:08:33 -0800

    Initiate Repository

---
+++ desktop/src/com/silferein/tennis/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.silferein.tennis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.silferein.tennis.DesktopTennis;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DesktopTennis(), config);
+	}
+}
