df532beb6119666869238e5faa6af75d1e4e4179
John Lynn <lynnjonathanp@gmail.com>
2015-11-27 16:29:19 -0800

    Add OrthographicCamera

--- core/src/com/silferein/tennis/DesktopTennis.java
+++ core/src/com/silferein/tennis/DesktopTennis.java
@@ -3,15 +3,20 @@ package com.silferein.tennis;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DesktopTennis extends ApplicationAdapter {
+	private OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+		
 		batch = new SpriteBatch();
 		img = new Texture("ball.png");
 	}
@@ -21,8 +26,10 @@ public class DesktopTennis extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 300, 250);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
